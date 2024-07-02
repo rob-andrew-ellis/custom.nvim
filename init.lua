d4a280e2e2bf2614a28de6c4bedb5b2bcffd8ce4
rob-andrew-ellis <robandrewellis@gmail.com>
2024-07-02 12:20:23 +0100

    Turned on spell checking (zf to add words to dict)

--- init.lua
+++ init.lua
@@ -35,6 +35,10 @@ vim.opt.breakindent = true
 -- Save undo history
 vim.opt.undofile = true
 
+-- Spelling
+vim.opt.spelllang = 'en_gb'
+vim.opt.spell = true
+
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
