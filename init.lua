2270279c6dfcd353b11c909b9fa7581595e6bdce
robertellis-digica <robert.ellis@digica.com>
2024-07-29 09:17:24 +0100

    made fuzzy find wrap preview so that it is readable

--- init.lua
+++ init.lua
@@ -267,11 +267,9 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          wrap_results = true,
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -1,3 +1,10 @@
 vim.cmd 'let g:netrw_liststyle = 3'
 vim.cmd 'set linebreak'
 vim.cmd 'autocmd FileType markdown setlocal spell'
+
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'TelescopePreviewerLoaded',
+  callback = function()
+    vim.wo.wrap = true
+  end,
+})
