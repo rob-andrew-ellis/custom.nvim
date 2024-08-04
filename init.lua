9f216bcb041dcf1019660b599cf745a75567ef5a
rob_andrew_ellis <robandrewellis@gmail.com>
2024-08-04 20:32:11 +0100

    Removed lint plugin as it had an error with markdown files

--- init.lua
+++ init.lua
@@ -826,7 +826,6 @@ require('lazy').setup({
   require 'custom.plugins.vim-table-mode',
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.indent_line',
-  require 'kickstart.plugins.lint',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
