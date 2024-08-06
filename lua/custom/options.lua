100002344c50321b7db75329a6be271a284c8fae
rob-andrew-ellis <robandrewellis@gmail.com>
2024-08-06 11:10:51 +0100

    Removed redundant keybinds

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -2,16 +2,6 @@ vim.cmd 'let g:netrw_liststyle = 3'
 vim.cmd 'set linebreak'
 vim.cmd 'autocmd FileType markdown setlocal spell'
 
--- vim.keymap.set('n', 'i', '<Up>', { noremap = true })
--- vim.keymap.set('n', 'k', '<Down>', { noremap = true })
--- vim.keymap.set('n', 'j', '<Left>', { noremap = true })
--- vim.keymap.set('n', 'l', '<Right>', { noremap = true })
--- vim.keymap.set('n', 'h', '<insert>', { noremap = true })
--- vim.keymap.set('n', 'J', '<Home>', { noremap = true })
--- vim.keymap.set('n', 'L', '<End>', { noremap = true })
--- vim.keymap.set('n', 'I', 'gg', { noremap = true })
--- vim.keymap.set('n', 'K', 'G', { noremap = true })
-
 vim.api.nvim_create_autocmd('User', {
   pattern = 'TelescopePreviewerLoaded',
   callback = function()
