64b8dd6094a3b2feb2826fa220d9a51a2b350e6e
rob_andrew_ellis <robandrewellis@gmail.com>
2024-08-07 17:16:26 +0100

    set x to replace current char with 'x' for more efficient checkbox usage

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -2,6 +2,8 @@ vim.cmd 'let g:netrw_liststyle = 3'
 vim.cmd 'set linebreak'
 vim.cmd 'autocmd FileType markdown setlocal spell'
 
+vim.keymap.set('n', 'x', '<delete>ix<escape>', { noremap = true })
+
 vim.api.nvim_create_autocmd('User', {
   pattern = 'TelescopePreviewerLoaded',
   callback = function()
