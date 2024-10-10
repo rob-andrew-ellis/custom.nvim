be22db35e9283616252e6fa1ceb3d288f2d8d753
rob-andrew-ellis <robandrewellis@gmail.com>
2024-10-10 22:24:56 +0100

    Set default tab width to 4

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -1,6 +1,10 @@
-vim.cmd 'let g:netrw_liststyle = 3'
 vim.cmd 'set linebreak'
 vim.cmd 'autocmd FileType markdown setlocal spell'
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 
 vim.api.nvim_create_autocmd('User', {
   pattern = 'TelescopePreviewerLoaded',
